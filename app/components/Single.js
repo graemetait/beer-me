a4120256ac7408d3a45e04a149e25522c7b9610d
Graeme Tait <burriko@gmail.com>
2016-04-19 14:59:10 +0100

    Load and display beer

--- app/components/Single.js
+++ app/components/Single.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import Loader from './Loader';
 
 const Single = React.createClass({
+  getInitialState() {
+    return {
+      beer: { name: '' },
+      loading: true
+    }
+  },
+  componentWillMount() {
+    this.loadBeer(this.props.params.beerId);
+  },
+  loadBeer(beerId) {
+    console.log(`Loading beer ${beerId}`);
+
+    this.setState({ loading: true });
+
+    fetch(`http://api.react.beer/v2/beer/${beerId}`)
+      .then(data => data.json())
+      .then(beer => {
+        this.setState({ beer: beer.data, loading: false });
+      });
+  },
   render() {
-    return <div className="single-beer">A single beer goes here</div>
+    if (this.state.loading) {
+      return <Loader message="Pouring a cold one!" />
+    }
+
+    return (
+      <div className="single-beer">
+        <h2>{this.state.beer.name}</h2>
+        <p>{this.state.beer.style.description}</p>
+      </div>
+    )
   }
 });
 

28e03ef28b7fcfc694d843b87c65b66666f9ce45
Graeme Tait <burriko@gmail.com>
2016-04-19 14:30:00 +0100

    List beers

--- app/components/Beer.js
+++ app/components/Beer.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import slug from 'slug';
+import { Link } from 'react-router';
 
 const Beer = React.createClass({
   render() {
-    return <div className="beer">{this.props.details.name}</div>
+    const { name, labels, id } = this.props.details;
+    return (
+      <div className="beer">
+        <Link to={`/beer/${id}/${slug(name)}`}>
+          <h2>{name}</h2>
+          <img src={labels.medium} />
+        </Link>
+      </div>
+    )
   }
 });
 
